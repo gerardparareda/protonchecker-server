9840d8e03ccda86b242feb315dce45ca7776f488
Gerard Parareda Gallifa <gerard.parareda@students.salle.url.edu>
2021-01-10 16:52:36 +0100

    Add settings file to run server and make petition to the Steam User API

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require("express");
+var fs = require('fs');
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var https = require("https");
+var axios = require('axios');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+
+let userId;
+
+let rawdata = fs.readFileSync("./settings.json");
+let settings = JSON.parse(rawdata);
 
 var router = express.Router();
 
@@ -14,8 +20,26 @@ router.get('/', function(req, res) {
    res.send("Hello World!");
 });
 
+app.get('/user/:userName', function (req, res) {
+    //https://wiki.teamfortress.com/wiki/WebAPI/ResolveVanityURL
+    let getIdURL = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${settings.steamApiKey}&vanityurl=${req.params.userName}`;
+
+    axios.get(getIdURL)
+    .then((response) => {
+      userId = response.data.response.steamid;
+      let getGamesURL = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${settings.steamApiKey}&steamid=${userId}format=json&include_appinfo=true`;
+
+      axios.get(getGamesURL)
+      .then((response) => {
+        let steamData = response.data;
+  
+        res.send(steamData);
+      });
+    });
+})
+
 app.use(router);
 
-app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+app.listen(settings.port, function() {
+  console.log(`Node server running on http://localhost:${settings.port}`);
 });
